7c88cc97c16209f6683a0c45161353f204fe2508
Lukasz Kita <lukasz.radoslaw.kita@cern.ch>
2021-09-23 13:45:14 +0200

    Delegate some functionalities into automation-control

--- RecoPPS/RPixEfficiencyTools/automation/EfficiencyAnalysisEngine.py
+++ RecoPPS/RPixEfficiencyTools/automation/EfficiencyAnalysisEngine.py
@@ -29,80 +29,9 @@ dataset = "/Charmonium/Run2018B-12Nov2019_UL2018-v1/AOD"
 template_for_first_module = "CrabConfigTemplateForFirstModule.py"
 template_for_second_module = "CrabConfigTemplateForSecondModule.py"
 
-class AnyClass():
-    """Class for performing generic comparision
-       This class allows defining two types of objects: 
-       1) an object which will always return True in case of 
-          comparing any other object with this object (this happens when no argument is passed to the constructor)
-       2) an object which will return True if the type of an object being compared
-          is one of the types specified while constructing the comparison object, False otherwise
-    """
-    def __eq__(self, other: Any)->bool:
-        """Used for operator-style comparison ('==' operator)
-
-        Args:
-            other (Any): object to be compared
-
-        Returns:
-            bool: True if no types where specified in the constructor or when the type of the object being compared
-                  is one of the types specified in the constructor, False otherwise
-        """
-        if not self.types: 
-            return True
-        for t in self.types:
-            if t==type(other):
-                return True
-        return False
-
-    def __init__(self, *types):
-        """Constructor of the class, allows to create and object of 1) and 2) type
-
-        Args:
-            *args: List of types 
-        """
-        self.types=types
-
-AnyInt = AnyClass(int)
 
 
-def define_status_enum(TaskStatusClass: Type[enum.Enum])->Type[enum.Enum]:
-    """Decorator used for marking the given class as the status enum and adding special __statuses__ member 
-       which is the list of all the enum fields defined within it.
-       The class provided as an input must inherit from enum.Enum class
-
-    Args:
-        TaskStatusClass (Type[enum.Enum]): class to be marked as status enum
-
-    Returns:
-        Type[enum.Enum]: class marked as status enum, with __statuses__ special member
-    """
-    TaskStatusClass.__statuses__ = TaskStatusClass.__members__.keys()
-    return TaskStatusClass
-
-
-def decorate_with_enum(TaskStatusClass: Type[Any])->Type[Any]:
-    """Decorator for defining the TaskStatus class  
-        
-        Usage:
-            decorate_with_enum(<enum status class defined with @define_status_enum decorator>)
-       
-        As a result we get the class consisting of all the user defined fields which were present in the input class
-        and the status enum fields defined in given enum status class.
-        The result class, apart from the __statuses__ special field has another special field - __allmembers__
-        consisting of all user-defined member fields (both from the input class and the enum status class)
-    """
-    def wrapper(AdditionalClass: Type[enum.Enum]):
-        TaskStatusClass.__allmembers__=[status for status in TaskStatusClass.__statuses__]
-        for attr in AdditionalClass.__dict__:
-            if not attr.startswith("_"):
-             
-                setattr(TaskStatusClass, attr, AdditionalClass.__dict__[attr])
-                TaskStatusClass.__allmembers__.append(attr)
-        return TaskStatusClass
-    return wrapper
-
-
-@define_status_enum
+@ctrl.define_status_enum
 class TaskStatusEnum(enum.Enum):
     """
         Class to encode enum tasks statuses for the purpouse of this automation workflow
@@ -121,7 +50,7 @@ class TaskStatusEnum(enum.Enum):
     done = enum.auto()
     
 
-@decorate_with_enum(TaskStatusEnum)
+@ctrl.decorate_with_enum(TaskStatusEnum)
 class TaskStatus:
     loop_id = 0.0    
     condor_job_id = 0
@@ -159,31 +88,6 @@ def process_new_tasks(tasks_list_path, task_controller):
 
 def flatten(t):
      return [item for sublist in t for item in sublist]
-   
-   
-def is_already_transfered(campaign, workflow, dataPeriod):
-    try:
-        process = subprocess.Popen(["crab", "status", "crab_"+"_".join([campaign, workflow, str(dataPeriod)]), "--proxy=/afs/cern.ch/user/l/lkita/public/lkita_proxy.x509"], stdout=subprocess.PIPE)
-        stdout = process.communicate()[0]
-
-        stdout = str(stdout, "utf-8")
-        
-        status = list(filter(lambda x: x!='', flatten(map(lambda x: x.split("\t"), stdout.split("\n")[8].split(" ")))))[2]
-        
-        if status=="finished":
-            return True
-        return False
-    except Exception as e:
-        logger.warning("Exception thrown: "+str(e))
-        return False
-    
-    
-def get_status(task_information, TaskStatusClass):
-    
-    for member_name in TaskStatusClass.__statuses__:
-        if task_information.get(member_name)==1:
-            return member_name
-    raise Exception("Task is in neither of the states defined in TaskStatusClass")
 
 
 def submit_task_to_crab(campaign, workflow, data_period, dataset, template):
@@ -267,71 +171,25 @@ def wait_for_condor(campaign, workflow, data_period):
 TRANSITIONS_DICT = {
                         'initialized': (submit_task_to_crab, 0, set_status_after_first_worker_submission, [dataset, template_for_first_module] ),
                         'duringFirstWorker': (ctrl.check_if_crab_task_is_finished, True, TaskStatus.waitingForFirstWorkerTransfer, []),
-                        'waitingForFirstWorkerTransfer': (is_already_transfered, True, TaskStatus.duringFirstHarvester, []),
-                        'duringFirstHarvester': (submit_task_to_condor, AnyInt, set_status_during_first_harvester, []),
+                        'waitingForFirstWorkerTransfer': (ctrl.is_crab_output_already_transfered, True, TaskStatus.duringFirstHarvester, []),
+                        'duringFirstHarvester': (submit_task_to_condor, ctrl.AnyInt, set_status_during_first_harvester, []),
                         'waitingForFirstHarvester': (wait_for_condor, True, TaskStatus.afterFirstHarvester, []),
                         'afterFirstHarvester': (submit_task_to_crab, 0, TaskStatus.duringSecondWorker, [dataset, template_for_second_module] ),
                         'duringSecondWorker': (ctrl.check_if_crab_task_is_finished, True, TaskStatus.waitingForSecondWorkerTransfer, []),
-                        'waitingForSecondWorkerTransfer': (is_already_transfered, True, TaskStatus.duringSecondHarvester, []),
-                        'duringSecondHarvester': (submit_task_to_condor, AnyInt, set_status_during_second_harvester, []),
+                        'waitingForSecondWorkerTransfer': (ctrl.is_crab_output_already_transfered, True, TaskStatus.duringSecondHarvester, []),
+                        'duringSecondHarvester': (submit_task_to_condor, ctrl.AnyInt, set_status_during_second_harvester, []),
                         'waitingForSecondHarvester': (wait_for_condor, True, TaskStatus.done, [])
                    } 
 
 
-def perform_action(task_information: ctrl.TaskCtrl.TaskInformationType, task_controller: ctrl.TaskCtrl.TaskControl, TaskStatusClass: ctrl.TaskCtrl.TaskStatusClassType)->Union[None, ctrl.TaskCtrl.TaskInformationType]:
-    """Attempts to proceed on automation workflow defined within TRANSITIONS_DICT
-
-    Args:
-        task_information (ctrl.TaskCtrl.TaskInformationType): current task status 
-        task_controller (ctrl.TaskCtrl.TaskControl): reference to task controller created for the purpouse of database connection etc.
-        TaskStatusClass (ctrl.TaskCtrl.TaskStatus): task status class - class defined with decorate_with_enum() decorator (should be corresponding to the task_information record)
-
-    Returns:
-        Union[None, ctrl.TaskCtrl.TaskInformationType]: New TaskInformationType with the updated status after the successfull operation, None otherwise
-    """
-    try:   
-        
-        func, expected_result, next_status, parms = TRANSITIONS_DICT[get_status(task_information, TaskStatusClass)]
-        logger.info("Method: %s will be performed on: (campaign: %s, workflow: %s, data period: %s)" %(func.__name__, task_information['campaign'], task_information['workflow'], task_information['dataPeriod']))
-        result = func(task_information['campaign'], task_information['workflow'], task_information['dataPeriod'], *parms)
-        
-        if result==expected_result:
-            logger.info("The operation result is the same as expected.")
-            if callable(next_status):
-                logger.info("Setting status with status generator")
-                task_information = task_controller.setStatusWithStatusGenerator(task_information['dataPeriod'], next_status, result)
-            else:
-                logger.info("Setting status as: "+str(next_status))
-                task_information=task_controller.setStatus(task_information['dataPeriod'], next_status)
-            return task_information
-        return None
-    except Exception as e:
-        logger.warning("Exception thrown: "+str(e))
-        return None
-    
-
-def process_tasks(task_controller: ctrl.TaskCtrl.TaskControl, TaskStatusClass: ctrl.TaskCtrl.TaskStatusClassType)->None:
-    """Attempts to perform as many steps in automation control as possible. Stops if perform_action() returns None (meaning that its operation didn't finish successfully)
-
-    Args:
-        task_controller (ctrl.TaskCtrl.TaskControl): task controller managing database connection etc.
-        TaskStatusClass (ctrl.TaskCtrl.TaskStatusClassType): task status class - class defined with decorate_with_enum() decorator
-    """
-    
-    tasks_in_database_information_list = task_controller.getAllTasks().get_points()
-    for task_information in tasks_in_database_information_list:
-        logger.info("Processing task: "+str(task_information))
-        task_information_buffer = task_information
-        while task_information_buffer != None and task_information_buffer['done']!=1:
-            task_information_buffer  = perform_action(task_information_buffer, task_controller, TaskStatusClass=TaskStatusClass)
-    
     
 if __name__ == '__main__':
     parser = prepare_parser()
     opts = parser.parse_args()
     task_controller = ctrl.TaskCtrl.TaskControl(campaign=campaign, workflow=workflow, TaskStatusClass=TaskStatus)
     process_new_tasks(opts.tasks_list_path, task_controller)
-    process_tasks(task_controller, TaskStatusClass=TaskStatus)
+    finite_state_machine = ctrl.FiniteStateMachine(TRANSITIONS_DICT)
+    finite_state_machine.process_tasks(task_controller, TaskStatusClass=TaskStatus)
     
     
     
